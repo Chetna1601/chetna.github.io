979eb8cbcef869feb255da6a0390967ee22e1a7e
Chetna Warkade <CWarkade@agility.com>
2021-04-30 19:52:01 +0530

    webeditor change

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,10 @@
 import { Component } from '@angular/core';
+import { AngularEditorConfig } from '@kolkov/angular-editor';
+import { Subject } from 'rxjs';
+import {
+  debounceTime, distinctUntilChanged, switchMap
+} from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +13,63 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'WebEditor';
+  htmlContent = '';
+  showButton = true;
+  historyText: Content[] = [];
+  showhistory = false;
+
+  config: AngularEditorConfig = {
+    editable: true,
+    spellcheck: true,
+    height: '15rem',
+    minHeight: '5rem',
+    placeholder: 'Enter text here...',
+    translate: 'no',
+    defaultParagraphSeparator: 'p',
+    defaultFontName: 'Arial',
+    toolbarHiddenButtons: [
+      ],
+    customClasses: [
+      {
+        name: "quote",
+        class: "quote",
+      },
+      {
+        name: 'redText',
+        class: 'redText'
+      },
+      {
+        name: "titleText",
+        class: "titleText",
+        tag: "h1",
+      },
+    ]
+  };
+
+  txtQuery: string; // bind this to input with ngModel
+  txtQueryChanged: Subject<string> = new Subject<string>();
+
+  constructor() {
+    let changetext = this.txtQueryChanged.pipe(debounceTime(3000), distinctUntilChanged()).subscribe( model => {
+      let text : Content = {content: model, time: new Date()}
+      this.historyText.push(text);
+    })
+  }
+
+  onFieldChange(query:string){
+    this.txtQueryChanged.next(query);
+  }
+
+  onClick() {
+    this.showButton = false;
+  }
+
+  showChangeHistory() {
+    this.showhistory = true;
+  }
+}
+
+export interface Content {
+  content: string;
+  time: Date;
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { AngularEditorModule } from '@kolkov/angular-editor';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule, AngularEditorModule, HttpClientModule, BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
